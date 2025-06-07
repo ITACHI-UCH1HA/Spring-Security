1a71da2e038435e69468a77127065d80354c4aed
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2025-06-07 18:48:43 +0530

    first commit on the spring security.

---
+++ section01/src/main/java/com/springsecurity/section01/EasyBankBackend.java
@@ -0,0 +1,12 @@
+package com.springsecurity.section01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBankBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBankBackend.class, args);
+	}
+}
---
+++ section01/src/test/java/com/springsecurity/section01/EasyBankBackendTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.section01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankBackendTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
