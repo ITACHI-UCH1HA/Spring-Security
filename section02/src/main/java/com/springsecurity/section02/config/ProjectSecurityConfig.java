92cd980675564b3c2f9f4b3fab781dacc77eec01
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2025-06-09 00:00:48 +0530

    section 2 practiced on 08 june 2025

---
+++ section02/src/main/java/com/springsecurity/section02/config/ProjectSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.springsecurity.section02.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myaccount", "/cards", "/loans", "/balance").authenticated()
+                .requestMatchers("/notices", "/contact").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
---
+++ section02/src/main/java/com/springsecurity/section02/controller/BalanceController.java
@@ -0,0 +1,12 @@
+package com.springsecurity.section02.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+    @GetMapping("/balance")
+    public String getBalance() {
+        return "get balance details from the database";
+    }
+}
