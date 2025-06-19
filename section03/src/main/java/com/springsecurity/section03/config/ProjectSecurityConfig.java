98ebaf95af71e42269be7d531d1624d5d2a0a342
SUJIT KUMAR NAYAK <sujitkumarnayak131@gmail.com>
2025-06-19 23:29:24 +0530

    all the practice done on 19th june 2024

--- section03/src/main/java/com/springsecurity/section03/config/ProjectSecurityConfig.java
+++ section03/src/main/java/com/springsecurity/section03/config/ProjectSecurityConfig.java
@@ -2,12 +2,17 @@ package com.springsecurity.section03.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -23,16 +28,26 @@ public class ProjectSecurityConfig {
     }
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
+        UserDetails user = User
+                .withUsername("user")
+                .password("{noop}Sujit@980")
                 .roles("USER")
                 .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("12345")
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("{bcrypt}$2a$12$COtyVrJBXzaIzR80Lqsq2e6fyt7ZD61Pi2OCclEIYUD03NwzckLsW")
                 .roles("USER", "ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
